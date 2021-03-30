22aaa24b99a6cddc02d5476e5bddbcd0ac31864a
Alperen KAÇMAZ <alperen703.akm@gmail.com>
2021-03-30 23:32:47 +0300

    Navbar added with comments.

--- php/navbar.php
+++ php/navbar.php
@@ -1,11 +1,64 @@
-<nav>
-hi 
-highlight_file
-highlight_file
+    <!--
+        It works with header scripts.
+        Button active is a problem. I deactive all button active tags.
+        All svg tags has been deleted. 
+        All `href` changed to `#`
+    --->
 
-yeni yeni
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
 
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">NAME IS TEST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NAME IS TEST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NAME IS TEST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NAME IS TEST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NAME IS TEST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NAME IS TEST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NAME IS TEST</a>
+                    </li>
+                </ul>
 
-alperen alperen
+                <!-- 
+                    Without php TAGS !!!!
+                    php echo  $_SESSION['userName'] . " " . $_SESSION['userSurname']
+                    --->
+                <a class="navbar-brand" href="#"><b id="upper">USER NAME</b></a>
 
-</nav>
+                <ul class="navbar-nav mb-2 mb-lg-0 justify-content-right">
+                    <li class="nav-item">
+                        <a class="nav-link" onclick="logoutFun()">LOGOUT</a>
+                    </li>
+                </ul>
+                <script>
+
+                    //location dont have function!! It conneted to the logout.php.
+                    function logoutFun() {
+                        var bol = confirm("ARE YOU SURE TO LOG-OUT?");
+                        if (bol) {
+                            location = "";
+                        }
+                    }
+                </script>
+            </div>
+        </div>
+    </nav>
