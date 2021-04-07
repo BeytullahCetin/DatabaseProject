c5d77efbabfc75cd185f4e7f06df6220f976a2fe
Alperen KAÇMAZ <alperen703.akm@gmail.com>
2021-04-07 16:57:41 +0300

    unnecessary links have been deleted and edits made

--- php/login.php
+++ php/login.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/login/style.css">
 </head>
 
 <body>
@@ -26,20 +26,13 @@
 						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
 							<div class="text w-100">
 								<h2>Welcome to the system</h2>
-								<p>Don't have an account?</p>
-								<a href="#" class="btn btn-white btn-outline-white">Sign Up</a>
+								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, adipisci doloribus nostrum iure laboriosam deleniti aliquid soluta nihil repellendus illo dignissimos! Earum, iusto aliquid odit magni doloremque nesciunt accusamus quibusdam?</p>
 							</div>
 						</div>
 						<div class="login-wrap p-4 p-lg-5">
 							<div class="d-flex">
 								<div class="w-100">
 									<h3 class="mb-4">Sign In</h3>
-								</div>
-								<div class="w-100">
-									<p class="social-media d-flex justify-content-end">
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-									</p>
 								</div>
 							</div>
 
@@ -127,11 +120,9 @@
 		</div>
 	</section>
 
-	<script src="js/jquery.min.js"></script>
-	<script src="js/popper.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/main.js"></script>
-
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/popper.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../js/main.js"></script>
 </body>
-
 </html>
