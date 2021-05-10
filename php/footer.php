0b7cc0a0b88b579472a54f47576de3c0c012975f
Alperen KAÇMAZ <alperen703.akm@gmail.com>
2021-05-10 17:50:03 +0300

    footer created

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,19 @@
-
+<footer class="footer bg-dark mt-auto">
+    <div class="container">
+        <p class="text-muted text-center"><strong>Apartment Management System © 2020 - 2021</strong></p>
+        <p class="text-muted text-center"><em>Usefull Links</em></p>
+        <figure class="text-end">
+            <blockquote class="footer bg-dark mt-auto">
+                <p class="text-muted text-center">
+                    <em>
+                        <a href="https://github.com/BeytullahCetin/DatabaseProject" target="_blank" class="text-white text-decoration-none">GitHub</a> | |
+                        <a href="http://cse.akdeniz.edu.tr/" target="_blank" class="text-white text-decoration-none">AU-CENG</a>
+                    </em>
+                </p>
+            </blockquote>
+        </figure>
+    </div>
+</footer>
 </body>
+
 </html>
