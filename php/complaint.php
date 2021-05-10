15270d1e986c603b1b46cb5921739214fc3fd715
gizemmuratoglu <gizemsude34@gmail.com>
2021-05-10 18:26:38 +0300

    map is added in contact page

--- php/complaint.php
+++ php/complaint.php
@@ -1,12 +1,14 @@
 <?php
 include "header.php";
+include "dbconn2.php"
+
 ?>
 <div class="container col-md-5  my-5">
 
-    <form method="POST">
+    <form action="" method="POST">
         <div class="form-group my-3">
             <label for="exampleInputEmail1">Name</label>
-            <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="" value="<?php echo  $_SESSION['user']['userName']; ?>" readonly="">
+            <input type="text" name="name" class="form-control" aria-describedby="emailHelp" placeholder="" value="<?php echo  $_SESSION['user']['userName']; ?>" readonly="">
 
         </div>
         <div class="form-group my-3">
@@ -15,24 +17,53 @@ include "header.php";
 
         </div>
         <div>
-        <label for="exampleInputEmail1"> Complaint Title </label>
-        <select class="form-select" aria-label="Default select example" name="complaintTitle">
-        
-            <option value="1" >Garrage</option>
-            <option value="2">Other</option>
-        </select>
+            <label for="exampleInputEmail1"> Complaint Title </label>
+            <select class="form-select" aria-label="Default select example" name="complaintTitle">
+
+                <option value="1">Garrage</option>
+                <option value="2">Other</option>
+            </select>
         </div>
         <div class="form-group my-3">
             <label for="exampleInputEmail1">Complaint&Request</label>
 
-            <textarea class="form-control" rows="5"></textarea>
+            <textarea class="form-control" rows="5" name="complaintMessage" id="complaintMessage"></textarea>
         </div>
 
 
-        <button type="submit" class="btn btn-primary" name="complaint submit">Submit</button>
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
     </form>
 </div>
 
+<?php
+$a= $_POST['complaintTitle'];
+$b=$_POST['complaintMessage'];
+if (isset($_POST['submit'])) {
+    $userID = $_SESSION['userID'];
+    $kaydet = $conn2->prepare("INSERT INTO complaint set       
+		userID=:userID,
+		complaintTitle=:complaintTitle,
+		complaintMessage=:complaintMessage
+	");
+
+
+    $insert = $kaydet->execute(array(
+        'userID' => $userID,
+        'complaintTitle' =>$a,
+        'complaintMesssage' =>$b ,
+    ));
+
+    if ($insert) {
+
+        header("Location:complaint.php?durum=OK");
+        exit();
+    } else {
+        header("Location:complaint.php?durum=$insert");
+        exit();
+    }
+}
+
+?>
 
 <?php
 include "footer.php";
--- php/contact.php
+++ php/contact.php
@@ -7,12 +7,12 @@ include "header.php";
         <div class="row">
             <div class="col">
                 <div class="text-left my-5" style="padding-top: 60px;">
-                    <p style="font-size: 27px;"><b style="color: red;">ADRESS: </b> Sultan Murat, Emsal Sk. No:26, 34295 Küçükçekmece/İstanbul</p> <br>
+                    <p style="font-size: 27px;"><b style="color: red;">ADRESS: </b> Güzeloba, 2384. Sk. No:23, 98452 Muratpaşa/Antalya</p> <br>
                     <p style="font-size: 27px;"><b style="color: red;">NUMBER:</b> <a href="">0(212)7884111</a> </p>
                 </div>
             </div>
             <div class="col my-3">
-                <img height="500" width="100%" src="konum.png">
+            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1TffQ2tCsPeS47MJsSCtzeWQinsa31zFJ" width="640" height="480"></iframe>
 
             </div>
 
