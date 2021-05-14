9b14acd9828ee2b9774918d31d1d9b249753e7b0
Beytullah Çetin <pasaport_07@hotmail.com>
2021-05-14 16:57:07 +0300

    Resident can only see their own dues.

--- php/showDues.php
+++ php/showDues.php
@@ -4,127 +4,260 @@ include "header.php";
 
 <div class="container col-md-8 my-5">
 
-    <div class="accordion" id="accordionExample">
+    <?php
+    if ($_SESSION['user']['isAdmin'] == "admin") {
+    ?>
 
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Unpaid Dues
-                </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne">
-                <div class="accordion-body p-0">
-                    <table class="table table-dark table-striped">
-                        <thead>
-                            <tr>
-                                <th>Action</th>
-                                <th>Door No</th>
-                                <th>Name - Surname</th>
-                                <th>GSM</th>
-                                <th>Due Period</th>
-                                <th>Due Price</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
+        <div class="accordion" id="accordionExample">
 
-                        <tbody>
-                            <?php
-
-                            $query = "SELECT u.userName, u.userSurname, u.userGSM, f.doorNo, d.duePrice, d.duePeriot, ufd.userFlatDueID 
-                            FROM user u, flat f, userFlat uf, userFlatDue ufd, due d 
-                            WHERE u.userID = uf.userID AND uf.flatID = f.flatID AND uf.userFlatID = ufd.userFlatID AND ufd.dueID = d.dueID AND ufd.paymentDate IS NULL
-                            ORDER BY d.duePeriot DESC, f.doorNo";
-                            $result = mysqli_query($conn, $query);
-
-                            while ($row = mysqli_fetch_assoc($result)) {
-                            ?>
-
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Unpaid Dues
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne">
+                    <div class="accordion-body p-0">
+                        <table class="table table-dark table-striped">
+                            <thead>
                                 <tr>
-                                    <td>
-                                        <form action="payDueDB.php" method="POST">
-                                            <input type="hidden" name="ufdID" value="<?php echo $row['userFlatDueID']; ?>">
-                                            <input class="btn btn-success" type="submit" name="payDueSubmit" value="Pay">
-                                        </form>
-                                    </td>
-                                    <td><?php echo $row['doorNo']; ?></td>
-                                    <td><?php echo $row['userName'] . " " . $row['userSurname']; ?></td>
-                                    <td><?php echo $row['userGSM']; ?></td>
-                                    <td><?php echo $row['duePeriot']; ?></td>
-                                    <td><?php echo $row['duePrice']; ?></td>
-                                    <td>NOT PAID</td>
+                                    <th>Action</th>
+                                    <th>Door No</th>
+                                    <th>Name - Surname</th>
+                                    <th>GSM</th>
+                                    <th>Due Period</th>
+                                    <th>Due Price</th>
+                                    <th>Status</th>
                                 </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php
+
+                                $query = "SELECT u.userName, u.userSurname, u.userGSM, f.doorNo, d.duePrice, d.duePeriot, ufd.userFlatDueID 
+                    FROM user u, flat f, userFlat uf, userFlatDue ufd, due d 
+                    WHERE u.userID = uf.userID AND uf.flatID = f.flatID AND uf.userFlatID = ufd.userFlatID AND ufd.dueID = d.dueID AND ufd.paymentDate IS NULL
+                    ORDER BY d.duePeriot DESC, f.doorNo";
+                                $result = mysqli_query($conn, $query);
+
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                ?>
+
+                                    <tr>
+                                        <td>
+                                            <form action="payDueDB.php" method="POST">
+                                                <input type="hidden" name="ufdID" value="<?php echo $row['userFlatDueID']; ?>">
+                                                <input class="btn btn-success" type="submit" name="payDueSubmit" value="Pay">
+                                            </form>
+                                        </td>
+                                        <td><?php echo $row['doorNo']; ?></td>
+                                        <td><?php echo $row['userName'] . " " . $row['userSurname']; ?></td>
+                                        <td><?php echo $row['userGSM']; ?></td>
+                                        <td><?php echo $row['duePeriot']; ?></td>
+                                        <td><?php echo $row['duePrice']; ?></td>
+                                        <td>NOT PAID</td>
+                                    </tr>
 
 
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Paid Dues
-                </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo">
-                <div class="accordion-body p-0">
-                    <table class="table table-dark table-striped">
-                        <thead>
-                            <tr>
-                                <th>Door No</th>
-                                <th>Name - Surname</th>
-                                <th>GSM</th>
-                                <th>Due Period</th>
-                                <th>Due Price</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <?php
-
-                            $query = "SELECT u.userName, u.userSurname, u.userGSM, f.doorNo, d.duePrice, d.duePeriot 
-                            FROM user u, flat f, userFlat uf, userFlatDue ufd, due d 
-                            WHERE u.userID = uf.userID AND uf.flatID = f.flatID AND uf.userFlatID = ufd.userFlatID AND ufd.dueID = d.dueID AND ufd.paymentDate IS NOT NULL
-                            ORDER BY d.duePeriot DESC, f.doorNo";
-                            $result = mysqli_query($conn, $query);
-
-                            while ($row = mysqli_fetch_assoc($result)) {
-                            ?>
-
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Paid Dues
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo">
+                    <div class="accordion-body p-0">
+                        <table class="table table-dark table-striped">
+                            <thead>
                                 <tr>
-                                    <td><?php echo $row['doorNo']; ?></td>
-                                    <td><?php echo $row['userName'] . " " . $row['userSurname']; ?></td>
-                                    <td><?php echo $row['userGSM']; ?></td>
-                                    <td><?php echo $row['duePeriot']; ?></td>
-                                    <td><?php echo $row['duePrice']; ?></td>
-                                    <td>PAID</td>
+                                    <th>Door No</th>
+                                    <th>Name - Surname</th>
+                                    <th>GSM</th>
+                                    <th>Due Period</th>
+                                    <th>Due Price</th>
+                                    <th>Status</th>
                                 </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php
+
+                                $query = "SELECT u.userName, u.userSurname, u.userGSM, f.doorNo, d.duePrice, d.duePeriot 
+                    FROM user u, flat f, userFlat uf, userFlatDue ufd, due d 
+                    WHERE u.userID = uf.userID AND uf.flatID = f.flatID AND uf.userFlatID = ufd.userFlatID AND ufd.dueID = d.dueID AND ufd.paymentDate IS NOT NULL
+                    ORDER BY d.duePeriot DESC, f.doorNo";
+                                $result = mysqli_query($conn, $query);
+
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $row['doorNo']; ?></td>
+                                        <td><?php echo $row['userName'] . " " . $row['userSurname']; ?></td>
+                                        <td><?php echo $row['userGSM']; ?></td>
+                                        <td><?php echo $row['duePeriot']; ?></td>
+                                        <td><?php echo $row['duePrice']; ?></td>
+                                        <td>PAID</td>
+                                    </tr>
 
 
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Old Users Dues
-                </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree">
-                <div class="accordion-body">
-                    Old Users Dues
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Old Users Dues
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree">
+                    <div class="accordion-body">
+                        Old Users Dues
+                    </div>
                 </div>
             </div>
+
         </div>
 
-    </div>
+    <?php } else { ?>
+
+        <div class="accordion" id="accordionExample">
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Unpaid Dues
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne">
+                    <div class="accordion-body p-0">
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Action</th>
+                                    <th>Door No</th>
+                                    <th>Name - Surname</th>
+                                    <th>GSM</th>
+                                    <th>Due Period</th>
+                                    <th>Due Price</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php
+
+                                $query = "SELECT u.userID, u.userName, u.userSurname, u.userGSM, f.doorNo, d.duePrice, d.duePeriot, ufd.userFlatDueID 
+                            FROM user u, flat f, userFlat uf, userFlatDue ufd, due d 
+                            WHERE u.userID = uf.userID AND uf.flatID = f.flatID AND uf.userFlatID = ufd.userFlatID AND ufd.dueID = d.dueID AND
+                            u.userID = " . $_SESSION['user']['userID'] . " AND ufd.paymentDate IS NULL
+                            ORDER BY d.duePeriot DESC, f.doorNo";
+                                $result = mysqli_query($conn, $query);
+
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                ?>
+
+                                    <tr>
+                                        <td>
+                                            <form action="payDueDB.php" method="POST">
+                                                <input type="hidden" name="ufdID" value="<?php echo $row['userFlatDueID']; ?>">
+                                                <input class="btn btn-success" type="submit" name="payDueSubmit" value="Pay">
+                                            </form>
+                                        </td>
+                                        <td><?php echo $row['doorNo']; ?></td>
+                                        <td><?php echo $row['userName'] . " " . $row['userSurname']; ?></td>
+                                        <td><?php echo $row['userGSM']; ?></td>
+                                        <td><?php echo $row['duePeriot']; ?></td>
+                                        <td><?php echo $row['duePrice']; ?></td>
+                                        <td>NOT PAID</td>
+                                    </tr>
+
+
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Paid Dues
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo">
+                    <div class="accordion-body p-0">
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Door No</th>
+                                    <th>Name - Surname</th>
+                                    <th>GSM</th>
+                                    <th>Due Period</th>
+                                    <th>Due Price</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php
+
+                                $query = "SELECT u.userName, u.userSurname, u.userGSM, f.doorNo, d.duePrice, d.duePeriot 
+                            FROM user u, flat f, userFlat uf, userFlatDue ufd, due d 
+                            WHERE u.userID = uf.userID AND uf.flatID = f.flatID AND uf.userFlatID = ufd.userFlatID AND ufd.dueID = d.dueID AND 
+                            u.userID = " . $_SESSION['user']['userID'] . " AND ufd.paymentDate IS NOT NULL
+                            ORDER BY d.duePeriot DESC, f.doorNo";
+                                $result = mysqli_query($conn, $query);
+
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $row['doorNo']; ?></td>
+                                        <td><?php echo $row['userName'] . " " . $row['userSurname']; ?></td>
+                                        <td><?php echo $row['userGSM']; ?></td>
+                                        <td><?php echo $row['duePeriot']; ?></td>
+                                        <td><?php echo $row['duePrice']; ?></td>
+                                        <td>PAID</td>
+                                    </tr>
+
+
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Old Users Dues
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree">
+                    <div class="accordion-body">
+                        Old Users Dues
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    <?php } ?>
+
 
 </div>
 
