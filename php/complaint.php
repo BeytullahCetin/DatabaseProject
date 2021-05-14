59bec75a46c59ec86a5bc630a5a41d997fc349bf
Alperen KAÇMAZ <alperen703.akm@gmail.com>
2021-05-14 20:56:21 +0300

    modification added to complaint admin

--- php/complaint.php
+++ php/complaint.php
@@ -1,102 +1,81 @@
 <?php
 include "header.php";
 ?>
-<script text="javascript">
-    function formKontrol() {
-        var complaintMessage = document.getElementById("complaintMessage").value;
+<?php
+if ($_SESSION['user']['isAdmin'] == "admin") { ?>
+    <div class="container col-md-8 my-5">
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                    <th>Number</th>
+                    <th>NAME</th>
+                    <th>SURNAME</th>
+                    <th>GSM </th>
+                    <th>Complaint Title </th>
+                    <th>Complaint</th>
+                    <th>Operation</th>
+                </tr>
+                <?php
+                $sayı = 0;
 
-        if (complaintMessage.length == 0 || complaintMessage == null) {
+                $bilgilerisor = $conn2->prepare("SELECT * FROM  complaint c,user u WHERE u.userID=c.userID ORDER BY complaintDate DESC   ");
+                $bilgilerisor->execute();
 
-            alert("PLEASE ENTER A COMPLAINT.");
-        } else {
-            alert("YOUR COMPLAINT HAS BEEN RECEIVED.");
+                while ($bilgileriçek = $bilgilerisor->FETCH(PDO::FETCH_ASSOC)) {
+                    $sayı++; ?>
 
-        }
-    }
-</script>
-    <?php
-    if ($_SESSION['user']['isAdmin'] == "admin") { ?>
-        <div class="container col-md-8 my-5">
-            <table class="table table-dark table-striped">
-                <thead>
                     <tr>
-                        <th>Number</th>
-                        <th>NAME</th>
-                        <th>SURNAME</th>
-                        <th>GSM </th>
-                        <th>Complaint Title </th>
-                        <th>Complaint</th>
-                        <th>Operation</th>
+                        <td><?php echo $sayı; ?></td>
+                        <td><?php echo $bilgileriçek['userName']; ?></td>
+                        <td><?php echo $bilgileriçek['userSurname']; ?></td>
+                        <td><?php echo $bilgileriçek['userGSM']; ?></td>
+                        <td><?php echo $bilgileriçek['complaintTitle']; ?></td>
+                        <td><?php echo $bilgileriçek['complaintMessage']; ?></td>
+                        <td>
+                            <form action="complaintDB.php" method="POST">
+                                <input type="hidden" name="complaintID" value="<?php echo $bilgileriçek['complaintID']; ?>">
+                                <input class="btn btn-success" type="submit" name="delete" value="DELETE" onclick="return confirm('Are you sure?')">
+                            </form>
+                        </td>
                     </tr>
-                    <?php
-                    $sayı = 0;
 
-                    $bilgilerisor = $conn2->prepare("SELECT * FROM  complaint c,user u WHERE u.userID=c.userID ORDER BY complaintDate DESC   ");
-                    $bilgilerisor->execute();
+                <?php } ?>
 
-                    while ($bilgileriçek = $bilgilerisor->FETCH(PDO::FETCH_ASSOC)) {
-                        $sayı++; ?>
+            </thead>
 
+        </table>
 
+    </div>
+<?php } else { ?>
 
-                        <tr>
-                            <td><? echo $sayı; ?></td>
-                            <td><?php echo $bilgileriçek['userName']; ?></td>
-                            <td><?php echo $bilgileriçek['userSurname']; ?></td>
-                            <td><?php echo $bilgileriçek['userGSM']; ?></td>
-                            <td><?php echo $bilgileriçek['complaintTitle']; ?></td>
-                            <td><?php echo $bilgileriçek['complaintMessage']; ?></td>
-                            <td>
-                                <form action="complaintDB.php" method="POST">
-                                    <input type="hidden" name="complaintID" value="<?php echo $bilgileriçek['complaintID']; ?>">
-                                    <input class="btn btn-success" type="submit" name="delete"  value="DELETE" onclick="return confirm('Are you sure?')">
-                                </form>
-                            </td>
-                        </tr>
+    <div class="container col-md-5 my-5">
+        <form action="complaint.php" method="POST">
+            <div class="form-group my-3">
+                <input type="text" name="userName" id="userName" class="form-control" aria-describedby="emailHelp" value="<?php echo  $_SESSION['user']['userName']; ?>" readonly>
+                <label for="userName">Name</label>
 
-                    <?php } ?>
-                   
+            </div>
+            <div class="form-group my-3">
+                <input type="text" name="userSurname" id="userSurname" class="form-control" aria-describedby="emailHelp" value="<?php echo  $_SESSION['user']['userSurname']; ?>" readonly>
+                <label for="userSurname">Surname</label>
+            </div>
+            <div>
+                <select class="form-select" aria-label="Default select example" name="complaintTitle" id="complaintTitle">
+                    <option value="Garrage">Garrage</option>
+                    <option value="Other">Other</option>
+                </select>
+                <label for="complaintTitle">Complaint Title</label>
+            </div>
+            <div class="form-group my-3">
+                <label for="exampleInputEmail1">Complaint&Request</label>
 
-                </thead>
+                <textarea class="form-control" rows="5" name="complaintMessage" id="complaintMessage" required=''></textarea>
+            </div>
 
-            </table>
-
-        </div>
-    <?php
-    }else{
-
-    
-    ?>
- <div class="container col-md-5 my-5">
-    <form action="complaint.php" method="POST">
-        <div class="form-group my-3">
-            <label for="exampleInputEmail1">Name</label>
-            <input type="text" name="name" class="form-control" aria-describedby="emailHelp" placeholder="" value="<?php echo  $_SESSION['user']['userName']; ?>" readonly="">
-
-        </div>
-        <div class="form-group my-3">
-            <label for="exampleInputEmail1">Surname </label>
-            <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="" value="<?php echo  $_SESSION['user']['userSurname']; ?>" readonly="">
-
-        </div>
-        <div>
-            <label for="exampleInputEmail1"> Complaint Title </label>
-            <select class="form-select" aria-label="Default select example" name="complaintTitle">
-                <option value="Garrage">Garrage</option>
-                <option value="Other">Other</option>
-            </select>
-        </div>
-        <div class="form-group my-3">
-            <label for="exampleInputEmail1">Complaint&Request</label>
-
-            <textarea class="form-control" rows="5" name="complaintMessage" id="complaintMessage" required=''></textarea>
-        </div>
-
-
-        <button type="submit" class="btn btn-primary" onclick="formKontrol()" name="submit">Submit</button>
-        <button type="reset" class="btn btn-danger" name="reset">Reset</button>
-    </form>
-</div>
+            <button type="submit" class="btn btn-primary" onclick="formKontrol()" name="submit">Submit</button>
+            <button type="reset" class="btn btn-danger" name="reset">Reset</button>
+        </form>
+    </div>
 
 <?php }
 include 'dbconn2.php';
