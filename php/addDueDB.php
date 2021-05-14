675a0a9a6d187603053edca7addcb391a0aab968
Beytullah Çetin <pasaport_07@hotmail.com>
2021-05-15 00:56:37 +0300

    addDue and payDue bugs fixed.

--- php/addDueDB.php
+++ php/addDueDB.php
@@ -9,9 +9,19 @@ $duePrice = $_POST['duePrice'];
 $query = "INSERT INTO due (duePrice, duePeriot) VALUES ($duePrice, '$duePeriot')";
 
 if(mysqli_query($conn, $query)){
-    header("Location: addDue.php?dueSuccesfullyAdded");
+    $last_id = mysqli_insert_id($conn);
+
+    $query = "SELECT * FROM userFlat WHERE exitDate IS NULL";
+    $result = mysqli_query($conn, $query);
+    
+    while($row = mysqli_fetch_array($result)){
+        $query = "INSERT INTO userFlatDue (dueID, userFlatID) VALUES ($last_id, ".$row['userFlatID'].")";
+        if(mysqli_query($conn, $query)){
+            echo "başarılı";
+        }else{
+            echo "başarısız";
+        }
+    }
 }else{
     echo "Error!" . $sql . "<br>" . mysqli_error($conn);
 }
-
-?>
