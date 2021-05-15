84559af2a01bd79d14d06c1fb7cb62fc7e7eb78a
Alperen KAÇMAZ <alperen703.akm@gmail.com>
2021-05-15 16:01:20 +0300

    test

--- php/message.php
+++ php/message.php
@@ -1,9 +1,32 @@
-<?php include "header.php"?>
+<?php include "header.php" ?>
 
 <main>
-
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col col-lg-2">
+                1 of 3
+            </div>
+            <div class="col-md-auto">
+                Variable width content
+            </div>
+            <div class="col col-lg-2">
+                3 of 3
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                1 of 3
+            </div>
+            <div class="col-md-auto">
+                Variable width content
+            </div>
+            <div class="col col-lg-2">
+                3 of 3
+            </div>
+        </div>
+    </div>
 
 
 </main>
 
-<?php include "footer.php"?>
+<?php include "footer.php" ?>
